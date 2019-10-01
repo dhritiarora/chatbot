4c0f3ba43c9f1e62c53584ff70b3dc24e64b6818
Dhriti Arora <44192174+dhritiarora@users.noreply.github.com>
2019-10-01 14:40:37 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 const { ActivityHandler } = require('botbuilder');
-const gen='https://api.genderize.io/?name=peter';
 class EchoBot extends ActivityHandler {
     constructor() {
         super();
@@ -14,18 +12,16 @@ class EchoBot extends ActivityHandler {
             await next();
         });*/
          // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        this.onMessage(async (context, next) => {
-            await context.sendActivity(http.get(gen))
+         this.onMessage(async (context, next) => {
+            await context.sendActivity(`https://api.genderize.io/?name='${context}`)
             .then(response => {
                     console.log(response.data);
             })
             .catch(error => console.error('On get student error', error));//context.activity.text.split('').reverse().join(''));
 
             // By calling next() you ensure that the next BotHandler is run.
-             await next();
+            await next();
         });
-
-
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
